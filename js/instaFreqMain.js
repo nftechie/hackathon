2b0b5e14782f47e20b64c5d94786c3c87a1ac548
Mr. Computer <mrComputer@Mrs-MacBook-Pro.local>
2014-01-25 15:48:30 -0800

    structured data

--- js/instaFreqMain.js
+++ js/instaFreqMain.js
@@ -278,19 +278,50 @@ $(document).ready(function(){
     }
 
     function createPieChart(){
+
+	    var found = false;
+
     	for (var i = 0; i < mainDataArray.length; i++){
-	        		filtersArray.push(mainDataArray[i].filter);
-	        	}
-
-	        	var found = false;
-
-	        	for (var i = 0; i < filtersArray.length; i++){
-
+	        		// var tmpObj = new Object();
+	        		// tmpObj.filter = mainDataArray[i].filter;
+	        		// filtersArray.push(tmpObj);
 	        		found = false;
 	        		for (var j = 0; j < filtersData.length; j++){
 
-	        			if (filtersArray[i] == filtersData[j].filter){
+	        			if (mainDataArray[i].filter == filtersData[j].filter){
 	        				filtersData[j].count++;
+
+	        				for(var ii =0; ii < mainDataArray[i].tags.length;ii++){
+	        					var tagFound = false;
+		        				for(var k =0; k < filtersData[j].hashTags.length;k++){
+		        					if(mainDataArray[i].tags[ii] == filtersData[j].hashTags[k]){
+		        						filtersData[j].hashTags[k].count++;
+
+		        						imgObj = new Object();
+			        					imgObj.images = mainDataArray[i].images;
+			        					imgObj.likes = mainDataArray[i].likes;
+	        					
+										filtersData[j].hashTags[k].imgs.push(imgObj);		        						
+
+		        						tagFound=true;
+		        						break;
+		        					}
+		        				}
+		        				if(!tagFound){
+									var hashObj = new Object();
+			        				hashObj.tag = mainDataArray[i].tags[ii];
+			        				hashObj.count = 1;
+	        						hashObj.imgs = new Array();
+	        						imgObj = new Object();
+		        					imgObj.images = mainDataArray[i].images;
+		        					imgObj.likes = mainDataArray[i].likes;
+		        					
+		        					hashObj.imgs.push(imgObj);
+			        				//hashObj.imgs.push(mainDataArray[i].images);
+			        				filtersData[j].hashTags.push(hashObj); 
+				        		}
+	        				}
+
 	        				found = true;
 	        				break;
 	        			}
@@ -298,12 +329,36 @@ $(document).ready(function(){
 
 	        		if (!found){
 	        			var tmpObj = new Object();
-	        			if (filtersArray[i] != null){
-	        				tmpObj.filter = filtersArray[i];
+	        			if (mainDataArray[i].filter != null){
+	        				tmpObj.filter = mainDataArray[i].filter;
 	        				tmpObj.count = 1;
+	        				tmpObj.hashTags = new Array();
+
+	        				for(var j = 0; j < mainDataArray[i].tags.length;j++){
+								var hashObj = new Object();
+	        					hashObj.tag = mainDataArray[i].tags[j];
+	        					hashObj.count = 1;
+	        					hashObj.imgs = new Array();
+
+	        					imgObj = new Object();
+	        					imgObj.images = mainDataArray[i].images;
+	        					imgObj.likes = mainDataArray[i].likes;
+
+	        					hashObj.imgs.push(imgObj);
+	        				
+	        					tmpObj.hashTags.push(hashObj);
+	        				}
+
+
 	        				filtersData.push(tmpObj);
 	        			}	
 	        		}
+	    }
+
+
+	        	for (var i = 0; i < filtersArray.length; i++){
+
+	        		
 	        	}
 
 
@@ -354,59 +409,254 @@ $(document).ready(function(){
     }
 
     function createBarGraph(){
-    	var margin = {top: 20, right: 20, bottom: 30, left: 40},
-		    width = 960 - margin.left - margin.right,
-		    height = 500 - margin.top - margin.bottom;
 
-		var x = d3.scale.ordinal()
-		    .rangeRoundBands([0, width], .1);
+var margin = {top: 30, right: 120, bottom: 0, left: 120},
+    width = 960 - margin.left - margin.right,
+    height = 500 - margin.top - margin.bottom;
 
-		var y = d3.scale.linear()
-		    .range([height, 0]);
+var x = d3.scale.linear()
+    .range([0, width]);
 
-		var xAxis = d3.svg.axis()
-		    .scale(x)
-		    .orient("bottom");
+var barHeight = 20;
 
-		var yAxis = d3.svg.axis()
-		    .scale(y)
-		    .orient("left")
-		    .ticks(10, "%");
+var color = d3.scale.ordinal()
+    .range(["steelblue", "#ccc"]);
 
-		var svg = d3.select("#mainContainer").append("svg")
-		    .attr("width", width + margin.left + margin.right)
-		    .attr("height", height + margin.top + margin.bottom)
-		  .append("g")
-		    .attr("transform", "translate(" + margin.left + "," + margin.top + ")");
+var duration = 750,
+    delay = 25;
 
-			data = hashTagData; 
+var partition = d3.layout.partition()
+    .value(function(d) { return d.size; });
 
-		  x.domain(data.map(function(d) { return d.tag; }));
-		  y.domain([0, d3.max(data, function(d) { return d.count; })]);
+var xAxis = d3.svg.axis()
+    .scale(x)
+    .orient("top");
 
-		  svg.append("g")
-		      .attr("class", "x axis")
-		      .attr("transform", "translate(0," + height + ")")
-		      .call(xAxis);
+var svg = d3.select("body").append("svg")
+    .attr("width", width + margin.left + margin.right)
+    .attr("height", height + margin.top + margin.bottom)
+  .append("g")
+    .attr("transform", "translate(" + margin.left + "," + margin.top + ")");
 
-		  svg.append("g")
-		      .attr("class", "y axis")
-		      .call(yAxis)
-		    .append("text")
-		      .attr("transform", "rotate(-90)")
-		      .attr("y", 6)
-		      .attr("dy", ".71em")
-		      .style("text-anchor", "end")
-		      .text("Frequency");
+svg.append("rect")
+    .attr("class", "background")
+    .attr("width", width)
+    .attr("height", height)
+    .on("click", up);
 
-		  svg.selectAll(".bar")
-		      .data(data)
-		    .enter().append("rect")
-		      .attr("class", "bar")
-		      .attr("x", function(d) { return x(d.tag); })
-		      .attr("width", x.rangeBand())
-		      .attr("y", function(d) { return y(d.count); })
-		      .attr("height", function(d) { return height - y(d.count); });
+svg.append("g")
+    .attr("class", "x axis");
+
+svg.append("g")
+    .attr("class", "y axis")
+  .append("line")
+    .attr("y1", "100%");
+
+// d3.json("readme.json", function(error, root) {
+//   partition.nodes(root);
+//   x.domain([0, root.value]).nice();
+//   down(root, 0);
+// });
+console.log("filtersData");
+console.log(filtersData);
+var barChartData = "";
+var test = "test";
+
+barChartData = "{ 'name': '" + test + "', 'children': [";
+
+        for (var i = 0; i < filtersData.length; i++){
+          if (i == filtersData.length - 1){
+            barChartData = barChartData + "{'name': '" + filtersData[i].filter + "', 'size': " + filtersData[i].count + "}]}";
+			// barChartData = barChartData + "{'name': '" + filtersData[i].filter + "', 'children': [" + filtersData[i].count + "}]}";
+			
+			// for(var j = 0; j < hashTagData.length;j++){
+   //        		if (i == hashTagData.length - 1){
+   //        			if()
+   //        			barChartData = barChartData +
+   //        		}else{
+   //        			barChartData = barChartData +
+   //        		}
+			// }
+
+			// barChartData = filtersData[i].count + "}]}";
+
+          } else {
+            barChartData = barChartData + "{'name': '" + filtersData[i].filter + "', 'size': " + filtersData[i].count + "},";
+          } 
+        }
+        console.log(barChartData);
+        var root = eval("(" + barChartData + ")");
+        console.log(root);
+	  partition.nodes(root);
+	  x.domain([0, root.value]).nice();
+	  down(root, 0);
+
+
+function down(d, i) {
+  if (!d.children || this.__transition__) return;
+  var end = duration + d.children.length * delay;
+
+  // Mark any currently-displayed bars as exiting.
+  var exit = svg.selectAll(".enter")
+      .attr("class", "exit");
+
+  // Entering nodes immediately obscure the clicked-on bar, so hide it.
+  exit.selectAll("rect").filter(function(p) { return p === d; })
+      .style("fill-opacity", 1e-6);
+
+  // Enter the new bars for the clicked-on data.
+  // Per above, entering bars are immediately visible.
+  var enter = bar(d)
+      .attr("transform", stack(i))
+      .style("opacity", 1);
+
+  // Have the text fade-in, even though the bars are visible.
+  // Color the bars as parents; they will fade to children if appropriate.
+  enter.select("text").style("fill-opacity", 1e-6);
+  enter.select("rect").style("fill", color(true));
+
+  // Update the x-scale domain.
+  x.domain([0, d3.max(d.children, function(d) { return d.value; })]).nice();
+
+  // Update the x-axis.
+  svg.selectAll(".x.axis").transition()
+      .duration(duration)
+      .call(xAxis);
+
+  // Transition entering bars to their new position.
+  var enterTransition = enter.transition()
+      .duration(duration)
+      .delay(function(d, i) { return i * delay; })
+      .attr("transform", function(d, i) { return "translate(0," + barHeight * i * 1.2 + ")"; });
+
+  // Transition entering text.
+  enterTransition.select("text")
+      .style("fill-opacity", 1);
+
+  // Transition entering rects to the new x-scale.
+  enterTransition.select("rect")
+      .attr("width", function(d) { return x(d.value); })
+      .style("fill", function(d) { return color(!!d.children); });
+
+  // Transition exiting bars to fade out.
+  var exitTransition = exit.transition()
+      .duration(duration)
+      .style("opacity", 1e-6)
+      .remove();
+
+  // Transition exiting bars to the new x-scale.
+  exitTransition.selectAll("rect")
+      .attr("width", function(d) { return x(d.value); });
+
+  // Rebind the current node to the background.
+  svg.select(".background")
+      .datum(d)
+    .transition()
+      .duration(end);
+
+  d.index = i;
+}
+
+function up(d) {
+  if (!d.parent || this.__transition__) return;
+  var end = duration + d.children.length * delay;
+
+  // Mark any currently-displayed bars as exiting.
+  var exit = svg.selectAll(".enter")
+      .attr("class", "exit");
+
+  // Enter the new bars for the clicked-on data's parent.
+  var enter = bar(d.parent)
+      .attr("transform", function(d, i) { return "translate(0," + barHeight * i * 1.2 + ")"; })
+      .style("opacity", 1e-6);
+
+  // Color the bars as appropriate.
+  // Exiting nodes will obscure the parent bar, so hide it.
+  enter.select("rect")
+      .style("fill", function(d) { return color(!!d.children); })
+    .filter(function(p) { return p === d; })
+      .style("fill-opacity", 1e-6);
+
+  // Update the x-scale domain.
+  x.domain([0, d3.max(d.parent.children, function(d) { return d.value; })]).nice();
+
+  // Update the x-axis.
+  svg.selectAll(".x.axis").transition()
+      .duration(duration)
+      .call(xAxis);
+
+  // Transition entering bars to fade in over the full duration.
+  var enterTransition = enter.transition()
+      .duration(end)
+      .style("opacity", 1);
+
+  // Transition entering rects to the new x-scale.
+  // When the entering parent rect is done, make it visible!
+  enterTransition.select("rect")
+      .attr("width", function(d) { return x(d.value); })
+      .each("end", function(p) { if (p === d) d3.select(this).style("fill-opacity", null); });
+
+  // Transition exiting bars to the parent's position.
+  var exitTransition = exit.selectAll("g").transition()
+      .duration(duration)
+      .delay(function(d, i) { return i * delay; })
+      .attr("transform", stack(d.index));
+
+  // Transition exiting text to fade out.
+  exitTransition.select("text")
+      .style("fill-opacity", 1e-6);
+
+  // Transition exiting rects to the new scale and fade to parent color.
+  exitTransition.select("rect")
+      .attr("width", function(d) { return x(d.value); })
+      .style("fill", color(true));
+
+  // Remove exiting nodes when the last child has finished transitioning.
+  exit.transition()
+      .duration(end)
+      .remove();
+
+  // Rebind the current parent to the background.
+  svg.select(".background")
+      .datum(d.parent)
+    .transition()
+      .duration(end);
+}
+
+// Creates a set of bars for the given data node, at the specified index.
+function bar(d) {
+  var bar = svg.insert("g", ".y.axis")
+      .attr("class", "enter")
+      .attr("transform", "translate(0,5)")
+    .selectAll("g")
+      .data(d.children)
+    .enter().append("g")
+      .style("cursor", function(d) { return !d.children ? null : "pointer"; })
+      .on("click", down);
+
+  bar.append("text")
+      .attr("x", -6)
+      .attr("y", barHeight / 2)
+      .attr("dy", ".35em")
+      .style("text-anchor", "end")
+      .text(function(d) { return d.name; });
+
+  bar.append("rect")
+      .attr("width", function(d) { return x(d.value); })
+      .attr("height", barHeight);
+
+  return bar;
+}
+
+// A stateful closure for stacking bars horizontally.
+function stack(i) {
+  var x0 = 0;
+  return function(d) {
+    var tx = "translate(" + x0 + "," + barHeight * i * 1.2 + ")";
+    x0 += x(d.value);
+    return tx;
+  };
+}
 
     }
 
