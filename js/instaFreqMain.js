6ade062b082e0f683dc626d98290259b4c972c6f
Alex Wormuth <awormuth@usc.edu>
2014-01-25 19:42:02 -0800

    lots of stuff

--- js/instaFreqMain.js
+++ js/instaFreqMain.js
@@ -494,7 +494,7 @@ var svg = d3.select("#mainContainer").append("svg")
     .attr("width", width + margin.left + margin.right)
     .attr("height", height + margin.top + margin.bottom)
   .append("g")
-    .attr("transform", "translate(" + margin.left + "," + margin.top + ")");
+    .attr("transform", "translate(" + 0 + "," + margin.top + ")");
 
 svg.append("rect")
     .attr("class", "background")
